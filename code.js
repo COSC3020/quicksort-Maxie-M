53b94f38d02115de6a923a64ca54123f6b5e2c20
Maxie <144727967+Maxie-M@users.noreply.github.com>
2024-10-20 19:00:19 -0600

    Update code.js

--- code.js
+++ code.js
@@ -2,33 +2,54 @@
 //Maxie Machado 
 //QuickSort 
 
-function swap(array, indexA, indexB) 
+function swap(array, index1, index2) 
 {
-    let tmp = array[indexA];
-    array[indexA] = array[indexB];
-    array[indexB] = tmp;
+    const tmp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = tmp;
 }
 
 function partition(array, leftBound, rightBound) 
+{
+    const mid = Math.floor((start + end) /2);
+    const pivotValue = array[mid];
+
+    swap(array, mid, end);
+    let pivotIndex = start;
+
+    for (let j = start; j < end; j++)
+    {
+        if (array[j] < pivotValue)
+        {
+            swap(array, j, pivotIndex);
+            pivotIndex++;
+        }
+    }
+    swap(array, pivotIndex, end);
+    return pivotIndex;
+}
+
+function quickSort(array)
 {
     if (array.length < 2) 
     {
         return array;
     }
 
-    let boundaries = [[0, array.length - 1]] 
+    const ind = [[0, array.length -1]];
 
-    while (boundaries.length > 0)
+    while (ind.length > 0)
     {
-        let [low, high] = boundaries.pop();
+        const [low, high] = ind.pop();
 
         if (low < high) 
         {
-            let pivotIndex = partition(array, low, high);
+            const pivotIndex = partition(array, low, high);
 
-            boundaries.push([low, pivotIndex -1]);
-            boundaries.push([pivotIndex + 1, high]);
+            ind.push([low, pivotIndex -1]);
+            ind.push([pivotIndex + 1, high]);
         }
     }
     return array;
 }
+
